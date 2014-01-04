0e6c6ee92ad26f634bb7bb7de473e4fb3b22174d
Kaelan Fouwels <kfouwels@outlook.com>
2014-01-04 16:12:04 +0000

    header-ing

--- twapi.php
+++ twapi.php
@@ -2,10 +2,9 @@
 	require('lib/Twilio.php');
 	include ('apiKeys.php');
 
-	$handleDesired = 'kfo';
+	$handleDesired = '@kfouwels';
 	$userPhoneNumber = $StoredPhoneNumber; //from apiKeys.php
 	echo $userPhoneNumber."\n";
-
 
 	$twilio = new Services_Twilio($AccountSid, $AuthToken);
 	
@@ -13,20 +12,25 @@
 
 	while($handleCaught == False)
 	{
-		echo "Requesting b&nd page\n";
-		$headers = get_headers("http://twitter.com/".$handleDesired);
+		echo "Requesting b&nd page for ".$handleDesired."\n";
+		$url = "http://twitter.com/".$handleDesired."\n";		
+		echo $url;		
 
-		echo substr($headers[0], 9, 3)."\n";
-		if (substr($headers[0], 9, 3) == "404" || "301")
+		$headers = get_headers($url, 1);
+		echo $headers[0];
+
+		if(strpos($headers[0], "404") !== false)
 		{
 			$handleCaught = True;
 			echo "Handle Caught\n";
 
-			$sms = $twilio->account->messages->sendMessage($SourceNumber, $userPhoneNumber, ('Handle '.$handleDesired.' is now free'));
+			//$sms = $twilio->account->messages->sendMessage($SourceNumber, $userPhoneNumber, ('Handle '.$handleDesired.' is now free'));
 		}
-
-		echo "==\nsleeping for 300==\n\n";
-		sleep(300); //wait 5 minutes
+		else
+		{
+			echo "\nsleeping\n\n";
+			sleep(2); //wait 5 minutes
+		}
 	}
 
 	echo "\nEND - yay?\n";
