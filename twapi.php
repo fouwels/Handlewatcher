93fbdc5b8bcbb73adab87d965d7809e32cef474b
Kaelan Fouwels <kfouwels@outlook.com>
2014-01-04 14:37:49 +0000

    Twilio me

--- twapi.php
+++ twapi.php
@@ -2,15 +2,15 @@
 	require('lib/Twilio.php');
 	include ('apiKeys.php');
 
-	$handleDesired = "kfo";
+	$handleDesired = 'kfo';
 	$userPhoneNumber = $StoredPhoneNumber; //from apiKeys.php
-	echo $userPhoneNumber;
+	echo $userPhoneNumber."\n";
 
 
 	$twilio = new Services_Twilio($AccountSid, $AuthToken);
 	
 	
-	$sms = $twilio->account->messages->sendMessage($userPhoneNumber, "Handle @".$handleDesired." is now free");
+	$sms = $twilio->account->messages->sendMessage($SourceNumber, $userPhoneNumber, ('Handle '.$handleDesired.' is now free'));
 
-	echo "END - yay?"
+	echo "\nEND - yay?"
 ?>
