ca76f55cbff9686ba75ef1ec16efd3a354056763
Kaelan Fouwels <kfouwels@outlook.com>
2014-01-04 15:15:16 +0000

    Dump page and strpos

--- twapi.php
+++ twapi.php
@@ -9,8 +9,40 @@
 
 	$twilio = new Services_Twilio($AccountSid, $AuthToken);
 	
-	
-	$sms = $twilio->account->messages->sendMessage($SourceNumber, $userPhoneNumber, ('Handle '.$handleDesired.' is now free'));
+	$handleCaught = False;
 
-	echo "\nEND - yay?"
+	while($handleCaught == False)
+	{
+
+		echo "\nRequesting b&nd page";
+		$headers = get_headers("http://twitter.com/".$handleDesired);
+		
+		if (substr($headers[0], 9, 3) == "404")
+		{
+			$sms = $twilio->account->messages->sendMessage($SourceNumber, $userPhoneNumber, ('Handle '.$handleDesired.' is now free'));
+		}
+
+		//$page = file_get_contents("http://twitter.com/".$handleDesired);
+		//$page = file_get_contents("http://twitter.com/asdadsukneariulgnelgr");
+		//if (strpos($page, "Sorry, that page doesn’t exist!") == True)
+		//{
+		//	echo "\nPage does not exist!";
+		//	$sms = $twilio->account->messages->sendMessage($SourceNumber, $userPhoneNumber, ('Handle '.$handleDesired.' is now free'));
+		//	$handleCaught = True;
+		//}
+
+
+
+
+
+
+		echo "\nsleeping for 300";
+		sleep(300); //wait 5 minutes
+
+
+	}
+
+	echo "\nEND - yay?";
+
+
 ?>
