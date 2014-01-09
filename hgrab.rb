9d2dfed594b78f0fd1d71aa8ac6c4eb24b096a68
Kaelan Fouwels <kfouwels@outlook.com>
2014-01-09 18:54:48 +0000

    nope

--- hgrab.rb
+++ hgrab.rb
@@ -1,28 +1,32 @@
 #!/usr/bin/env ruby
 require 'uri'
 require 'twilio-ruby'
+require 'net/http'
 require 'httparty'
 require './apiKeys.rb'
 
-def quickCheckPage(handle)
-  url = 'http://www.twitter.com/' + handle
-  if !url.nil?
-    puts 'checking: ' + url
-    puts 'status -> ' + HTTParty.get('http://www.example.com/', :headers => {'User-Agent' => 'test'}).code.to_s
-  end
-end
+#def checkPage(handle)      #https? #user-agent? # No - still going to give me a 301...
+#  url = 'https://www.twitter.com/' + handle
+#  if !url.nil?
+#    puts 'checking: ' + url
+#    uri = URI(url)
+#    http = Net::HTTP.new(uri.host, 443)      #WORST. HTTP. CLASS. EVA.
+#    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#    http.use_ssl = true
+#    req = Net::HTTP::Get.new(uri.request_uri)
+#    req['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0'
+#    puts http.request(req).code
+#    return http.request(req).code
+#  end
+#end
 
-def checkPage(handle)
-  url = 'http://www.twitter.com/' + handle
+def partyCheckPage(handle)
+  url = 'http://www.twitter.com/' + handle + '/'
   if !url.nil?
     puts 'checking: ' + url
-    uri = URI(url)
-    http = Net::HTTP.new(uri.host, 443)
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http.use_ssl = true
-    req = Net::HTTP::Get.new(uri.request_uri)
-    req['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0'
-    puts http.request(req).code
+    status = HTTParty.get(url , :headers => {'User-Agent' => 'test'}).code.to_s
+    puts 'status -> ' + status
+    return status
   end
 end
 
@@ -32,7 +36,8 @@ def pingUser(message)
   puts 'Pinged user' + resp.sid
 end
 
-checkPage('kfouwels')
-#pingUser('Yo')
+if partyCheckPage('kfouwels') == 200
+  pingUser('Handle seems to be available')
+end
 
 puts 'END'
