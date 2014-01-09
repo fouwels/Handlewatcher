7181e6c37ffdf5f5de6fd0285fa8968f63c7d84e
Kaelan Fouwels <kfouwels@outlook.com>
2014-01-09 18:21:45 +0000

    HTTParty time!

--- hgrab.rb
+++ hgrab.rb
@@ -1,24 +1,14 @@
 #!/usr/bin/env ruby
-require 'net/http'
 require 'uri'
-require './apiKeys.rb'
 require 'twilio-ruby'
-
-#def checkPage(handle)
-#  url = 'http://www.twitter.com/' + handle
-#  if !url.nil?
-#    puts 'checking: ' + url
-#    puts 'status -> ' + Net::HTTP.get_response(URI(url)).code      #Fine, 301 me then
-#  end
-#end
+require 'httparty'
+require './apiKeys.rb'
 
 def checkPage(handle)
   url = 'http://www.twitter.com/' + handle
   if !url.nil?
     puts 'checking: ' + url
-    request = Net::HTTP::Get.new(URI(url))
-    request.add_field('User-Agent', 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36')
-
+    puts 'status -> ' + HTTParty.get('http://www.example.com/', :headers => {'User-Agent' => 'HTTParty'}).code.to_s
   end
 end
 
@@ -29,6 +19,6 @@ def pingUser(message)
 end
 
 checkPage('kfouwels')
-pingUser('Yo')
+#pingUser('Yo')
 
 puts 'END'
