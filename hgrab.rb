82173f4937365ac52c44e09ae28da8d6f56bb08d
Kaelan Fouwels <kfouwels@outlook.com>
2014-01-09 18:07:08 +0000

    User agent problem?

--- hgrab.rb
+++ hgrab.rb
@@ -4,11 +4,21 @@ require 'uri'
 require './apiKeys.rb'
 require 'twilio-ruby'
 
+#def checkPage(handle)
+#  url = 'http://www.twitter.com/' + handle
+#  if !url.nil?
+#    puts 'checking: ' + url
+#    puts 'status -> ' + Net::HTTP.get_response(URI(url)).code      #Fine, 301 me then
+#  end
+#end
+
 def checkPage(handle)
   url = 'http://www.twitter.com/' + handle
   if !url.nil?
     puts 'checking: ' + url
-    puts 'status -> ' + Net::HTTP.get_response(URI(url)).code      #Fine, 301 me then
+    request = Net::HTTP::Get.new(URI(url))
+    request.add_field('User-Agent', 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36')
+
   end
 end
 
