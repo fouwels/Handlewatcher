b8d6516b94b8c36d70b56baf3c16e2346970abcf
Kaelan Fouwels <kfouwels@outlook.com>
2014-01-09 18:44:38 +0000

    HTTPS, bad user-agent?

--- hgrab.rb
+++ hgrab.rb
@@ -4,11 +4,25 @@ require 'twilio-ruby'
 require 'httparty'
 require './apiKeys.rb'
 
+def quickCheckPage(handle)
+  url = 'http://www.twitter.com/' + handle
+  if !url.nil?
+    puts 'checking: ' + url
+    puts 'status -> ' + HTTParty.get('http://www.example.com/', :headers => {'User-Agent' => 'test'}).code.to_s
+  end
+end
+
 def checkPage(handle)
   url = 'http://www.twitter.com/' + handle
   if !url.nil?
     puts 'checking: ' + url
-    puts 'status -> ' + HTTParty.get('http://www.example.com/', :headers => {'User-Agent' => 'HTTParty'}).code.to_s
+    uri = URI(url)
+    http = Net::HTTP.new(uri.host, 443)
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0'
+    puts http.request(req).code
   end
 end
 
